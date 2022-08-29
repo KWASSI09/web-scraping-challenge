062f1dff5a8c7d6975236415179ba35a1573f411
Kouame Kouassi <javi.marius.kj@gmail.com>
2022-08-29 12:20:35 +0100

    still working

--- app.py
+++ app.py
@@ -6,24 +6,25 @@ import scrape_mars
 app = Flask(__name__)
 
 # setup mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/MarsDB")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Mars_app"
+mongo = PyMongo(app)
 
 
 
 @app.route("/")
-def home():
+def index():
     
-    # write a statement that finds all the items in the db and sets it to a variable
-    nasa_mars_data=mongo.db.mars_data.find_one()
+    # finds all the items in the db and sets it to a variable
+    nasa_mars_data=mongo.db.nasa_mars_data.find_one()
     # render an index.html template and pass it the data you retrieved from the database
-    return render_template("index.html", data=nasa_mars_data)
+    return render_template("index.html", nasa_mars_data=nasa_mars_data)
 
 @app.route("/scrape")
 def scrape(): 
-    
-    nasa_mars_data =scrape_mars.scrape() 
-    mongo.db.mars_data.update({}, nasa_mars_data, upsert=True)
-    return redirect("/")
+    nasa_mars_data =mongo.db.nasa_mars_data
+    mars_data =scrape_mars.scrape()
+    nasa_mars_data.update({}, mars_data, upsert=True)
+    return redirect("/",code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
